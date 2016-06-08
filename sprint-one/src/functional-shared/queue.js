0b6576c535e482c3ddc9061f5652d039fd5546ad
juliusbuckley <juliusbuckley@gmail.com>
2016-06-08 15:31:33 -0700

    set up functional-shared instantiation pattern.

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,8 +1,27 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var queueInstance = {};
+  queueInstance.storage = {};
+  queueInstance.head = 0;
+  queueInstance.tail = 0;
+
+  _.extend(queueInstance, queueMethods);
+
+  return queueInstance;
 };
 
 var queueMethods = {};
 
+queueMethods.enqueue = function(val) {
+  this.storage[this.tail++] = val;
+};
+
+queueMethods.dequeue = function() {
+
+};
+
+queueMethods.size = function() {
+
+};
 
