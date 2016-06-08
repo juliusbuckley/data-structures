9819e7c72759e99546a661a517d497fd3ff2009b
juliusbuckley <juliusbuckley@gmail.com>
2016-06-08 15:37:19 -0700

    Completed prompts for functional-shared instantiation.

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,25 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var stackInstance = {};
+  stackInstance.storage = {};
+  stackInstance.index = 0;
+  _.extend(stackInstance, stackMethods);
+  return stackInstance;
 };
 
 var stackMethods = {};
+
+stackMethods.push = function(val) {
+  
+};
+
+stackMethods.pop = function() {
+
+};
+
+stackMethods.size = function() {
+
+};
 
 
