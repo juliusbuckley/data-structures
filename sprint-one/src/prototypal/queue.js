f9617bc12e489a77e5f2592312cedc40d4e13018
juliusbuckley <juliusbuckley@gmail.com>
2016-06-08 16:31:02 -0700

    Created prototypal instantiation style for stack and queue.

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,22 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var queueInstance = Object.create(queueMethods);
+  queueInstance.storage = {};
+  queueInstance.head = 0;
+  queueInstance.tail = 0;
+  return queueInstance;
 };
 
 var queueMethods = {};
 
+queueMethods.enqueue = function(val) {
+  this.storage[this.tail++] = val;
+};
+queueMethods.dequeue = function() {
+
+};
+queueMethods.size = function() {
+
+};
 
--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,25 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var stackInstance = Object.create(stackMethods);
+  stackInstance.storage = {};
+  stackInstance.index = 0;
+  return stackInstance;
 };
 
 var stackMethods = {};
+stackMethods.push = function(val) { 
+  this.storage[this.index++] = val;
+};
 
+stackMethods.pop = function() { 
+  this.size() && this.index--;
+  var temp = this.storage[this.index];
+  delete this.storage[this.index];
+  return temp;
+};
+
+stackMethods.size = function() { 
+  return this.index;
+};
 
