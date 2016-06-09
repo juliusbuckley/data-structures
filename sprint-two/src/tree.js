063c53d00add85d8192604ad51ef853c2481c2e4
juliusbuckley <juliusbuckley@gmail.com>
2016-06-08 20:35:59 -0700

    Completed prompts for tree.

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -3,17 +3,29 @@ var Tree = function(value) {
   newTree.value = value;
 
   // your code here
-  newTree.children = null;  // fix me
-
+  newTree.children = [];
+  _.extend(newTree, treeMethods);
   return newTree;
 };
 
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
+  this.children.push(Tree(value));
 };
 
-treeMethods.contains = function(target) {
+treeMethods.contains = function(target, currentLocation) {
+  currentLocation = currentLocation || this;
+  if (currentLocation.value === target) {
+    return true;
+  } else {
+    for (var i = 0; i < currentLocation.children.length; i++) {
+      if (this.contains(target, currentLocation.children[i])) {
+        return true;
+      }
+    }
+    return false;
+  }
 };
 
 
