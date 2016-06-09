55544b724a0ab530036149bea0340fab9dc24a70
juliusbuckley <juliusbuckley@gmail.com>
2016-06-09 10:48:11 -0700

    Completed all prompts for binary search tree.

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,4 +1,59 @@
 var BinarySearchTree = function(value) {
+  var treeInstance = {};
+  treeInstance.left = null;
+  treeInstance.right = null;
+  treeInstance.value = value;
+  _.extend(treeInstance, treeMethods);
+  return treeInstance;
+};
+
+var treeMethods = {};
+
+treeMethods.insert = function(val, currentNode) {
+  currentNode = currentNode || this;
+  if (val < currentNode.value) {
+    if (!currentNode.left) {
+      currentNode.left = BinarySearchTree(val);
+    } else {
+      currentNode.insert(val, currentNode.left);
+    }
+  } else if (val > currentNode.value) {
+    if (!currentNode.right) {
+      currentNode.right = BinarySearchTree(val);
+    } else {
+      currentNode.insert(val, currentNode.right);
+    }
+  }
+};
+
+treeMethods.contains = function(target, currentNode) {
+  currentNode = currentNode || this;
+  if (currentNode.value === target) {
+    return true;
+  } else if (target < currentNode.value) {
+    if (currentNode.left === null) {
+      return false;
+    } else {
+      return this.contains(target, currentNode.left);
+    }
+  } else if (target > currentNode.value) {
+    if (currentNode.right === null) {
+      return false;
+    } else {
+      return this.contains(target, currentNode.right);
+    }
+  }
+};
+
+treeMethods.depthFirstLog = function(cb, currentNode) {
+  currentNode = currentNode || this;
+  cb(currentNode.value);
+  if (currentNode.left) {
+    currentNode.depthFirstLog(cb, currentNode.left);
+  } 
+  if (currentNode.right) {
+    currentNode.depthFirstLog(cb, currentNode.right);
+  }
 };
 
 
