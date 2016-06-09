88d2b667721f4d0c425ed186c4c8d462652d8e79
juliusbuckley <juliusbuckley@gmail.com>
2016-06-09 16:40:38 -0700

    Add hash table functionality for manipulating tuples.

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -5,17 +5,27 @@ var HashTable = function() {
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
-  this._storage.set(index, v);
+  if (this._storage.get(index) === undefined) {
+    this._storage.set(index, []);
+  } 
+  this._storage.get(index).push([k, v]);
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
-  return this._storage.get(index);
+  return this._storage.get(index).filter(function(tuple){
+    return tuple[0] === k;
+  })[0][1]; 
 };
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
-  delete this._storage[index];
+  var valueToRemove = this._storage.get(index);
+  this._storage.each(function (item, i, list) {
+    if (valueToRemove === item) {
+      delete list[i];
+    }
+  });
 };
 
 
