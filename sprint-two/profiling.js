00c800c74f4b2b1111c1902b2a371c8eacea1302
juliusbuckley <juliusbuckley@gmail.com>
2016-06-10 11:41:30 -0700

    Implement doubly linked list.

---
+++ sprint-two/profiling.js
@@ -0,0 +1,7 @@
+var binarySearchTree = BinarySearchTree(5);
+
+for (var i = 0; i < 100; i++) {
+  binarySearchTree.insert(i);
+  console.log('inserted ', i)
+}
+
---
+++ sprint-two/src/doublyLinkedList.js
@@ -0,0 +1,69 @@
+var DoublyLinkedList = function() {
+  this.storage = {};
+  this.head = null;
+  this.tail = null;
+};
+
+var DoubleNode = function(val) {
+  this.value = val;
+  this.next = null;
+  this.prev = null;
+};
+
+DoublyLinkedList.prototype.addToTail = function (value) {
+  if (this.head === null) {
+    this.head = this.tail = new DoubleNode(value);
+  } else {
+    var temp = this.tail;
+    this.tail.next = new DoubleNode(value);
+    this.tail = new DoubleNode(value);
+    this.tail.prev = temp;
+  }
+};
+
+DoublyLinkedList.prototype.removeHead = function () {
+  var temp = this.head;
+  if (this.head.next) {
+    this.head = this.head.next;
+    this.head.prev = null;    
+  }
+  return this.head.value;
+};
+
+DoublyLinkedList.prototype.contains = function (target, currentNode) {
+  currentNode = currentNode || this.head;
+  if (currentNode.value === target) {
+    return true;
+  }
+
+  if (currentNode.next) {
+    if (this.contains(target, currentNode.next)) {
+      return true;
+    } 
+  }
+
+  return false;
+  
+
+}; 
+
+DoublyLinkedList.prototype.addToHead = function (value) {
+  if (this.head === null) {
+    this.head = this.tail = new DoubleNode(value);
+  } else {
+    var temp = this.head;
+    this.head.next = new DoubleNode(value);
+    this.head = new DoubleNode(value);
+    this.head.prev = temp;
+  }
+};
+
+DoublyLinkedList.prototype.removeTail = function (target) {
+  var temp = this.tail;
+  if (this.tail.prev) {
+    this.tail = this.tail.prev;
+    this.tail.next = null;    
+  }
+  return this.tail.value;
+
+};
